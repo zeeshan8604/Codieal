6918738da47bc0faa7c3b0ad2a07e47010402c0f
Zeeshan Ahmed <zeeshanahmed860443@gmail.com>
2023-04-06 21:09:18 +0530

    first commit:create the directory ans setup express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app=express();
+const port=8000;
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log('error in running port:', port);
+    }
+    console.log('server running on port:', port);
+})
